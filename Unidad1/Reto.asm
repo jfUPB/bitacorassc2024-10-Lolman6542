36766c151797afc6ae9f9e3d95cdc61d42d34b42
Lolman6542 <127360762+Lolman6542@users.noreply.github.com>
2024-05-15 14:29:39 -0500

    Reto Hecho.asm
    Aca se encuentra el codigo hecho.

--- Unidad1/Reto.asm
+++ Unidad1/Reto.asm
@@ -1,54 +1,33 @@
 (Inicio)
-@80 // Tecla P
-D=A
-@KBD // Se refiere al espacio memoria 24576 en el teclado
-D=D-M
-@CocinandoBucle
-D;JEQ
-@90 // Tecla Z
+@80
 D=A
 @KBD
 D=D-M
-@CocinandoBucleA // Bucle para pintar en blanco
+@preparandoBucle
 D;JEQ
 
-(BorrarPantalla)
-@SCREEN // Se refiere al espacio de memoria 16384 para la pantalla
+@90
+D=A
+@KBD
+D=D-M
+@preparandoBuclee
+D;JEQ
+
+(PintarBlanco)
+@SCREEN
 M=0
 @Inicio
 0;JMP
 
 (PintarNegro)
-@SCREEN // Se refiere al espacio que pintara en la memoria 16384 para la pantalla
+@SCREEN
 M=-1
-@Inicio // No necesitamos volver al inicio, ya que queremos que el cambio sea permanente.
-0;JMP
-
-(CocinandoBucle)
-@24576
-D=A
-@suma // Revisa si llego al final de bucle
-M=D
-@16384 // Se guarda en SCREEN
-D=A
-@variable // Posicion del bit de la pantalla
-M=D
-
-(PintarNegroBucle)
-@variable
-A=M
-M=-1
-@variable
-M=M+1
-D=M
-@suma
-D=M-D
-@PintarNegroBucle
-D;JGT
 @Inicio
 0;JMP
 
-(CocinandoBucleA)
+
+(preparandoBucle)
+
 @24576
 D=A
 @suma
@@ -58,8 +37,35 @@ D=A
 @variable
 M=D
 
-(BorrarPantallaBucle)
+(PintarNegroBucle)
 @variable
+
+A=M
+M=-1
+@variable
+M=M+1
+D=M
+@suma
+D=M-D
+
+@PintarNegroBucle
+D;JGT
+@Inicio
+0;JMP
+
+(preparandoBuclee)
+@24576
+D=A
+@suma
+M=D
+@16384
+D=A
+@variable
+M=D
+
+(PintarBlancoBucle)
+@variable
+
 A=M
 M=0
 @variable
@@ -67,7 +73,8 @@ M=M+1
 D=M
 @suma
 D=M-D
-@BorrarPantallaBucle
-D;JGT // Vuelve al inicio
+
+@PintarBlancoBucle
+D;JGT
 @Inicio
 0;JMP
